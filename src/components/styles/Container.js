96b28ea95cf99feee2431bcfc14157044620e45d
mac <mac@macui-MacBookPro.local>
2023-07-12 18:38:22 +0900

    7월12일 오후6시

--- src/components/styles/Container.js
+++ src/components/styles/Container.js
@@ -1,14 +1,10 @@
 import styled from 'styled-components'
 
-// export const Container = styled.div` 
-//   width:100%;
-//   max-width: 1920px;
-//   background-color: red;
-  // /* border: 1px solid black; */
 
-// `
+
+
+
+
 export const Container = styled.div`
-  /* width:100%;
-  max-width: 1920px; */
   border: 1px solid black;
 `
